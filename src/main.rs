93e98f5b953211e3b8cd6ebc5574401c856179b7
Christian Jaeger <ch@christianjaeger.ch>
2023-03-19 01:27:13 +0100

    Add example from the docs, comment out tracing_subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,125 @@
+use {egui_miniquad as egui_mq, miniquad as mq};
+
+struct Stage {
+    egui_mq: egui_mq::EguiMq,
+    show_egui_demo_windows: bool,
+    egui_demo_windows: egui_demo_lib::DemoWindows,
+    color_test: egui_demo_lib::ColorTest,
+    pixels_per_point: f32,
+}
+
+impl Stage {
+    fn new(ctx: &mut mq::Context) -> Self {
+        Self {
+            egui_mq: egui_mq::EguiMq::new(ctx),
+            show_egui_demo_windows: true,
+            egui_demo_windows: Default::default(),
+            color_test: Default::default(),
+            pixels_per_point: ctx.dpi_scale(),
+        }
+    }
+}
+
+struct MyMiniquadApp {
+    egui_mq: egui_miniquad::EguiMq,
+}
+
+impl MyMiniquadApp {
+    fn new(ctx: &mut mq::Context) -> Self {
+        Self {
+            egui_mq: egui_miniquad::EguiMq::new(ctx),
+        }
+    }
+}
+
+impl mq::EventHandler for MyMiniquadApp {
+    fn update(&mut self, _: &mut mq::Context) {}
+
+    fn draw(&mut self, mq_ctx: &mut mq::Context) {
+        mq_ctx.clear(Some((1., 1., 1., 1.)), None, None);
+        mq_ctx.begin_default_pass(mq::PassAction::clear_color(0.0, 0.0, 0.0, 1.0));
+        mq_ctx.end_render_pass();
+
+        self.egui_mq.run(mq_ctx, |_mq_ctx, egui_ctx|{
+            egui::Window::new("Egui Window").show(egui_ctx, |ui| {
+                ui.heading("Hello World!");
+            });
+        });
+
+        // Draw things behind egui here
+
+        self.egui_mq.draw(mq_ctx);
+
+        // Draw things in front of egui here
+
+        mq_ctx.commit_frame();
+    }
+
+    fn mouse_motion_event(&mut self, _: &mut mq::Context, x: f32, y: f32) {
+        self.egui_mq.mouse_motion_event(x, y);
+    }
+
+    fn mouse_wheel_event(&mut self, _: &mut mq::Context, dx: f32, dy: f32) {
+        self.egui_mq.mouse_wheel_event(dx, dy);
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        mb: mq::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.egui_mq.mouse_button_down_event(ctx, mb, x, y);
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        mb: mq::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.egui_mq.mouse_button_up_event(ctx, mb, x, y);
+    }
+
+    fn char_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        character: char,
+        _keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        self.egui_mq.char_event(character);
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        keycode: mq::KeyCode,
+        keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        self.egui_mq.key_down_event(ctx, keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut mq::Context, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+        self.egui_mq.key_up_event(keycode, keymods);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // #[cfg(not(target_arch = "wasm32"))]
+    // {
+    //     // Log to stdout (if you run with `RUST_LOG=debug`).
+    //     tracing_subscriber::fmt::init();
+    // }
+
+    let conf = mq::conf::Conf {
+        high_dpi: true,
+        window_width: 1200,
+        window_height: 1024,
+        ..Default::default()
+    };
+    mq::start(conf, |mut ctx| Box::new(Stage::new(&mut ctx)));
 }
